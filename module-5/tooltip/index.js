a5ff366d31e549b6da685d7cd81d012153a9142a
sanwebm <sanwebm@yandex.ru>
2020-04-16 13:12:15 +0300

    'Tooltip

--- module-5/tooltip/index.js
+++ module-5/tooltip/index.js
@@ -11,21 +11,70 @@ class Tooltip {
     Tooltip.instance = this;
   }
 
+  pointerOver(event){
+    this.render(event.x, event.y, event.target.dataset.tooltip);
+  }
+
+  pointerOut(){
+    if(this.element){
+      this.destroy();
+    }
+  }
+  pointerMove(event){
+    if(this.element){
+      this.element.style.left = event.x + 10 + 'px';
+      this.element.style.top = event.y + 10  + 'px';
+    }
+  }
+
   initEventListeners () {
+    document.addEventListener('pointerover', event => {
+      if(event.target.dataset.tooltip) {
+        this.pointerOver(event);
+      }
+
+      document.addEventListener('pointerout', event => {
+        if(event.target.dataset.tooltip){
+          this.pointerOut();
+        }
+      });
+
+      document.addEventListener('pointermove', event => {
+        if(event.target.dataset.tooltip){
+          this.pointerMove(event);
+        }
+      });
+
+    });
+
 
   }
 
   initialize () {
-
+    this.initEventListeners();
   }
 
-  render () {
-
+  render (x=0, y=0, message='message') {
+    this.element = document.createElement('div');
+    this.element.classList.add('tooltip');
+    this.element.style.top = `${y}px`;
+    this.element.style.left = `${x}px`;
+    this.element.textContent = `${message}`;
+    document.body.append(this.element);
   }
 
   destroy () {
+    if(this.element){
+      this.element.remove();
+      this.element = null;
+    }
 
+    document.removeEventListener('pointerover', this.pointerOver);
+    document.removeEventListener('pointerout', this.pointerOut);
+    document.removeEventListener('pointermove', this.pointerMove);
   }
+
+
 }
 
 const tooltip = new Tooltip();
